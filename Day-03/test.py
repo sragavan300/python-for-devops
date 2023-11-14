8d0570e441741c48029a2d674ced31425bf9b754
sragavan300 <sragavan300@gmail.com>
2023-11-14 16:49:22 -0500

    featureday03 changes

---
+++ Day-03/test.py
@@ -0,0 +1,14 @@
+
+value1 = 5;
+value2 = 3;
+
+def addition():
+    print (value1 + value2);
+
+def sub():
+    value1 = 20
+    value2 = 15
+    print (value1 - value2);
+
+addition()
+sub()
---
+++ Day-03/test2.py
@@ -0,0 +1,4 @@
+import test
+val = test.addition(100, 200);
+
+print("Output:", val)
